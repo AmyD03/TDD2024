abb89b5601c80a621b018cf0705b626021b43a61
Amy <dingxiaoyu168@outlook.com>
2024-04-25 14:44:55 +0800

    13th commit

--- lists/tests.py
+++ lists/tests.py
@@ -28,11 +28,11 @@ class HomePageTest(TestCase):
     def test_displays_all_list_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
-
         response = self.client.get('/')
+        items = response.context['items']  # 获取上下文中的待办事项列表
 
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+        self.assertIn('itemey 1', [item.text for item in items])
+        self.assertIn('itemey 2', [item.text for item in items])
 
     # def test_root_url_resolves_to_home_page_view(self):
     #     found = resolve('/')
@@ -49,20 +49,12 @@ class HomePageTest(TestCase):
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.endswith('</html>'))
 
-    def test_uses_home_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
-
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         
-        #增加三行
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
-        
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'],'/')
     
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
