1532a6fb7169e895415b6406285187a1c6944159
Amy <dingxiaoyu168@outlook.com>
2024-04-25 16:16:29 +0800

    16th commit

--- lists/views.py
+++ lists/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 from lists.models import Item
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-new-page/')
+    # if request.method == 'POST':
+    #     Item.objects.create(text=request.POST['item_text'])
+    #     return redirect('/lists/the-new-page/')
     
     # items = Item.objects.all()
     # return render(request, 'home.html',{'items':items})
@@ -15,3 +15,8 @@ def home_page(request):
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html',{'items':items})
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-new-page/')
--- notes/urls.py
+++ notes/urls.py
@@ -7,7 +7,7 @@ Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
+    1. Add an import:  from other_app.visews import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
@@ -25,5 +25,7 @@ from lists import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
-    path('lists/the-new-page/', views.view_list, name='view_list'),
+    path('lists/new', views.new_list, name='new_list'),
+    path('lists/the-new-page/', views.view_list, name='view_list')
+    
 ]
