c985d06deb75d4ddd08e2de332add9a04ac94f94
Amy <dingxiaoyu168@outlook.com>
2024-05-26 22:40:32 +0800

    30th commit

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+#import unittest
 from selenium.webdriver.common.by import By
 #from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
@@ -14,7 +15,33 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Chrome()
         real_server = os.environ.get('REAL_SERVER')
         if real_server:
-            self.live_server_url = 'http://' + real_server
+            self.live_server_url='http//'+real_server
+
+
+    def test_layout_and_styling(self):
+        #张三访问首页
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        #她看到输入框完美地居中显示
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x']+inputbox.size['width'] / 2 ,
+            512,
+            delta=10
+        )
+        #他新建了一个清单，看到输入框仍然完美地居中显示
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1:testing')
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x']+inputbox.size['width'] / 2 ,
+            512,
+            delta=10
+        )
+
+
 
     def tearDown(self):
         self.browser.quit()
@@ -24,114 +51,118 @@ class NewVisitorTest(StaticLiveServerTestCase):
         while True:
             try:
                 table = self.browser.find_element(By.ID,'id_list_table')
-                rows = table.find_elements(By.TAG_NAME, 'tr')
-                self.assertIn(row_text, [row.text for row in rows])
+                rows = table.find_elements(By.TAG_NAME,'tr')
+                self.assertIn(row_text,[row.text for row in rows])
                 return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT :
+            except(AssertionError,WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a cool new online to-do app. She goes
-        # to check out its homepage
-        self.browser.get(self.live_server_url)
-        
-      # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)
-        header_text=self.browser.find_element(By.TAG_NAME,'h1').text
-        self.assertIn('To-Do',header_text)
 
-        inputbox=self.browser.find_element(By.ID,'id_new_item')
+    
+        #张三听说有一个在线待办事项的应用
+        #他去看了这个应用的首页
+        #self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
+
+
+        #他注意到有to-do这个词
+        self.assertIn('To-Do',self.browser.title) #,"browser title was:" + self.browser.title
+        header_text = self.browser.find_element(By.TAG_NAME,'h1').text
+        self.assertIn('To-Do',header_text)
+        
+
+        #应用有一个输入待办事项的文本输入框
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
-        )
+            )
 
-        inputbox.send_keys('Buy flowers') #(2)
+        #他在文本输入框中输入了“Buy flowers"
+        inputbox.send_keys('Buy flowers')
 
-        #他按了回车键，页面更新了
-        #待办事项表格中显示了“1: Buy flowers”
+        #他按了回车键键后，页面更新了
+        #待办事项表格中显示了“1: Buy flowers"
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy flowers')
+        #time.sleep(1)
+        self.wait_for_row_in_list_table('1:Buy flowers')
 
-        #页面中又显示了一个文本框，可以输入其他的待办事项
-        #她输入了“Give a gift to Lisi”
+        #table = self.browser.find_element(By.ID,'id_list_table')
+        #rows = table.find_elements(By.TAG_NAME,'tr')
+        #self.assertIn('1: Buy flowers',[row.text for row in rows])
+
+        #页面中又显示了一个文本输入框，可以输入其他待办事项
+        #他输入了“Give a gift to Lisi"
         inputbox = self.browser.find_element(By.ID,'id_new_item')
         inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
+        #time.sleep(1)
 
-        self.wait_for_row_in_list_table('1: Buy flowers')
-        self.wait_for_row_in_list_table('2: Give a gift to Lisi')
+        #页面再次更新，她的清单中显示了这两个待办事项
+        # table = self.browser.find_element(By.ID,'id_list_table')
+        # rows = table.find_elements(By.TAG_NAME,'tr')
+        # self.assertIn('1: Buy flowers',[row.text for row in rows])
+        # self.assertIn('2: Give a gift to Lisi',[row.text for row in rows])
+        self.wait_for_row_in_list_table('1:Buy flowers')
+        self.wait_for_row_in_list_table('2:Give a gift to Lisi')
 
-        # self.fail('Finish the test!')
+
+        #张三想知道这个网站是否会记住他的清单
+        #他看到到网站为他生成了一个唯一的URL
+        #self.fail('Finish the test!')
+        #他访问那个URL
+        #发现他的待办事项列表还在
+
+
+        #他满意的离开了
+
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
-        #张三新建一个待办事项清单
+        # 张三新建一个待办事项清单
         self.browser.get(self.live_server_url)
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
         inputbox.send_keys('Buy flowers')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy flowers')
+        self.wait_for_row_in_list_table('1:Buy flowers')
 
-        #他注意到清单有一个唯一的URL
+        # 他注意到清单有个唯一的URL
         zhangsan_list_url = self.browser.current_url
-        self.assertRegex(zhangsan_list_url, '/lists/.+')
+        self.assertRegex(zhangsan_list_url,'/lists/.+') 
 
-        #现在有一个叫做王五的新用户访问了网站
-        ##我们使用一个新的浏览器会话
-        ##确保张三的信息不会从cookie中泄露出来
+        #现在一个新用户王五访问网站
+        #我们使用一个新刘览器会话
+        #确保张三的信息不会从cookle中泄露出去
         self.browser.quit()
         self.browser = webdriver.Chrome()
 
-        #王五访问首页
-        #首页中没有张三的待办事项
+        # 王五访问首页
+        #页面中看不到到张三的清单
         self.browser.get(self.live_server_url)
-        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
-        self.assertNotIn('Buy flowers', page_text)
-        self.assertNotIn('Send a gift to Lisi', page_text)
-
-        #王五输入一个新的待办事项，新建一个清单
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        page_text = self.browser.find_element(By.TAG_NAME,'body').text
+        self.assertNotIn('Buy fLowers',page_text)
+        self.assertNotIn('Give a gift to Lisi',page_text)
+        
+        
+        #王五输入一个新待办事项，新建一个清单
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy milk')
-
+        self.wait_for_row_in_list_table('1:Buy milk')
         #王五获得了他的唯一URL
         wangwu_list_url = self.browser.current_url
-        self.assertRegex(wangwu_list_url, '/lists/.+')
-        self.assertNotEqual(wangwu_list_url, zhangsan_list_url)
+        self.assertRegex(wangwu_list_url,'/lists/.+')
+        self.assertNotEqual(wangwu_list_url,zhangsan_list_url)
+        
 
-        #这个页面中还是没有张三的待办事项
-        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
-        self.assertNotIn('Buy flowers', page_text)
-        self.assertIn('Buy milk', page_text)
+        #这个页面还是没有张三的清单
+        page_text = self.browser.find_element(By.TAG_NAME,'body').text
+        self.assertNotIn('Buy flowers',page_text)
+        self.assertIn('Buy milk',page_text)
+        
+        #两人都很满意，然后去睡觉了
 
-        #两人都很满意，去睡觉了
-
-    def test_layout_and_styling(self):
-        #张三访问首页
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024,768)
-
-        #他看到输入框居中显示
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width']/2,
-            512,
-            delta=10
-        )
-
-        #他新建了一个清单，看到输入框仍然居中显示
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width']/2,
-            512,
-            delta=10
-        )
-
-# if __name__ == '__main__':
+# if __name__ =='__main__':
 #     unittest.main()
