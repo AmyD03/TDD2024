f633ac39f3081bd908a15a4d53437ee7805e1fc3
Amy <dingxiaoyu168@outlook.com>
2024-05-26 15:12:08 +0800

    Revert "27th fix some errors in the code"
    This reverts commit f7a0cec9e0e638d71aae50f3a22937685b4860bc.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -12,9 +12,9 @@ MAX_WAIT = 10
 class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
-        # real_server = os.environ.get('REAL_SERVER')
-        # if real_server:
-        #     self.live_server_url = 'http://' + real_server
+        real_server = os.environ.get('REAL_SERVER')
+        if real_server:
+            self.live_server_url = 'http://' + real_server
 
     def tearDown(self):
         self.browser.quit()
